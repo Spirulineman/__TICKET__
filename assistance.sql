8fd8285660ba8b76ac0a5d7fdf4db33f4e87c051
Spirulineman <remipointcom29@gmail.com>
2020-10-23 08:52:32 +0200

    dump SQL assistance BDD

---
+++ assistance.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2020 at 08:51 AM
+-- Server version: 5.7.17
+-- PHP Version: 7.1.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assistance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id_categorie` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categorie`
+--
+
+INSERT INTO `categorie` (`id_categorie`, `nom`) VALUES
+(1, 'voiture'),
+(2, 'vélo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reponse`
+--
+
+CREATE TABLE `reponse` (
+  `id_reponse` int(11) NOT NULL,
+  `contenu` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_support` int(11) NOT NULL,
+  `id_ticket` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reponse`
+--
+
+INSERT INTO `reponse` (`id_reponse`, `contenu`, `id_user`, `id_support`, `id_ticket`) VALUES
+(1, 'blavblablavblabl  avblablavblablav blablavblabl avblablavblablav  blablavblabla vblablavblablav blabla vblabla   vblablavbla', 1, 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `support`
+--
+
+CREATE TABLE `support` (
+  `id_support` int(11) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `support`
+--
+
+INSERT INTO `support` (`id_support`, `pseudo`, `mail`, `password`) VALUES
+(1, 'sophie stiquer', 'stiquer@estmalfait.com', '11111'),
+(2, 'stanley deroo-bignole', 'pairede@glawoui.com', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id_ticket` int(11) NOT NULL,
+  `numero` int(8) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `contenu` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_categorie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`id_ticket`, `numero`, `titre`, `contenu`, `id_user`, `id_categorie`) VALUES
+(1, 12345678, 'panne de voiture', 'panne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiturepanne de voiture', 1, 1),
+(2, 87654321, 'vélo déraillé', 'vélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillévélo déraillé', 2, 2),
+(5, 13245687, 'pneu crevé', 'pneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevé', 2, 1),
+(7, 54783214, 'kilo', 'pneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevépneu crevé', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `pseudo`, `mail`, `password`) VALUES
+(1, 'Jean', 'jean.neymar@detoutssa.com', '111111'),
+(2, 'jean qule', 'jean.qule@assec.com', '11111');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id_categorie`);
+
+--
+-- Indexes for table `reponse`
+--
+ALTER TABLE `reponse`
+  ADD PRIMARY KEY (`id_reponse`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_ticket` (`id_ticket`),
+  ADD KEY `id_support` (`id_support`);
+
+--
+-- Indexes for table `support`
+--
+ALTER TABLE `support`
+  ADD PRIMARY KEY (`id_support`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id_ticket`),
+  ADD UNIQUE KEY `numero` (`numero`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_categorie` (`id_categorie`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `reponse`
+--
+ALTER TABLE `reponse`
+  MODIFY `id_reponse` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `support`
+--
+ALTER TABLE `support`
+  MODIFY `id_support` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reponse`
+--
+ALTER TABLE `reponse`
+  ADD CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `reponse_ibfk_2` FOREIGN KEY (`id_ticket`) REFERENCES `ticket` (`id_ticket`),
+  ADD CONSTRAINT `reponse_ibfk_3` FOREIGN KEY (`id_support`) REFERENCES `support` (`id_support`);
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
